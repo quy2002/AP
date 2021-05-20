6ad12f0b864e217e830cc670a257b78059e999ee
quy2002 <trandinhquyzz2002@gmail.com>
2021-05-20 15:06:39 +0700

    Initial commit

---
+++ Baitap/Program.cs
@@ -0,0 +1,69 @@
+﻿using ManageProduct.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageProduct
+{
+    class Program
+    {
+        private static object i;
+
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+
+            int choose;
+            do
+            {
+                ShowMenu();
+                choose = int.Parse(Console.ReadLine() ?? string.Empty);
+
+
+                switch (choose)
+                {
+                    case 1:
+                        Console.WriteLine("Infomation Product : ");
+                        product.ShowInfo();
+
+                        break;
+                    case 2:
+                        Console.WriteLine("CheckQty");
+                        product.CheckQty();
+                        break;
+                    case 3:
+                        Console.WriteLine("Add Image");
+                        Console.WriteLine("Nhap 1 anh:");
+                        i = Console.ReadLine();
+                        product.AddImage(i.ToString());
+                        break;
+                    case 4:
+                        Console.WriteLine("4. Remove Image");
+                        Console.WriteLine("Nhap 1 anh can xoa:");
+                        i = Console.ReadLine();
+                        product.RemoveImage(i.ToString());
+                        break;
+                    case 5:
+                        Console.WriteLine("5. Exit ");
+                        break;
+                    default:
+                        Console.WriteLine("Nhap khong chinh xac ( chi nhap tu 1-5)");
+                        break;
+
+                }
+            } while (choose != 5);
+        }
+
+        static void ShowMenu()
+        {
+            Console.WriteLine("1.GetInfo");
+            Console.WriteLine("2.CheckQty");
+            Console.WriteLine("3.Add Image");
+            Console.WriteLine("4. Remove Image");
+            Console.WriteLine("5. Exit ");
+            Console.Write("Choose : ");
+        }
+    }
+}
---
+++ Baitap/Properties/Product.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageProduct.DTO
+{
+    public class Product
+    {
+
+        public Product()
+        {
+            id = 1;
+            name = "abc";
+            price = 3;
+            image = "cba";
+            desc = 1;
+            qty = 9;
+
+        }
+
+
+        public Product(int id, string name, int price, string image, int desc, int qty, List<string> gallery)
+        {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+            this.image = image;
+            this.desc = desc;
+            this.qty = qty;
+            this.gallery = gallery;
+        }
+
+        public int id { get; set; }
+        public string name { get; set; }
+        public int price { get; set; }
+        public string image { get; set; }
+        public int desc { get; set; }
+        public int qty { get; set; }
+        List<string> gallery = new List<string>();
+
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Ten SV: " + name);
+            Console.WriteLine("Tuoi: " + id);
+            Console.WriteLine("Ten SV: " + price);
+            Console.WriteLine("Tuoi: " + image);
+            Console.WriteLine("Ten SV: " + desc);
+            Console.WriteLine("Tuoi: " + qty);
+            Console.WriteLine("Tuoi: " + gallery);
+
+        }
+
+        public void CheckQty()
+        {
+            if (qty > 0)
+            {
+                Console.WriteLine("In stock");
+
+            }
+            Console.WriteLine("Out of stock");
+        }
+
+        public bool AddImage(string i)
+        {
+            if (gallery.Count >= 10)
+            {
+
+                Console.WriteLine("Qua 10 anh vui long xoa bot");
+                return false;
+            }
+            else
+            {
+                gallery.Add(i);
+                var b = gallery.Count();
+                Console.WriteLine("So anh trong list gallery {0}", b);
+                var nameImageinGallery = gallery.ToArray();
+                for (int q = 0; q < nameImageinGallery.Count(); q++)
+                {
+                    Console.WriteLine("Phan tu thu {0} trong mang? ten image : {1}", q, nameImageinGallery[q]);
+                }
+                return true;
+
+            }
+
+        }
+
+        public bool RemoveImage(string i)
+        {
+
+            gallery.Remove(i);
+            Console.WriteLine("da xoa anh");
+            return true;
+        }
+    }
+}
